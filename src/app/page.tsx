55799d089b142e82b4870c73be3e475e7f4b826e
ArcadeW <108231469+ArcadeW@users.noreply.github.com>
2024-06-06 13:22:00 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,3 @@
-import { cn } from "@/lib/utils";
-
-export default function CyberButton({
-  title,
-  symbol,
-  className,
-}: {
-  title: string;
-  symbol: string;
-  className?: string;
-}) {
-  return (
-    <button className={cn("cybr-btn", className)}>
-      {title}
-      <span aria-hidden>_</span>
-      <span aria-hidden className="cybr-btn__glitch">
-        {title}_
-      </span>
-      <span aria-hidden className="cybr-btn__tag">
-        {symbol}
-      </span>
-    </button>
-  );
-}
-
-import { cn } from "@/lib/utils";
-import styles from "./glitch.module.scss";
-import { distopianFont, inter } from "@/lib/font";
-import CyberButton from "../button";
-
-export default function GlitchSection({
-  text,
-  description,
-}: {
-  text: string;
-  description: string;
-}) {
-  return (
-    <div
-      className={cn(
-        "flex flex-col items-top justify-top min-h-screen",
-        distopianFont.className,
-        styles.body
-      )}
-    >
-      <div className={styles.glitch}>
-        {Array.from({ length: 9 }).map((_, index) => (
-          <div key={index} className={styles.line}>
-            {text}
-          </div>
-        ))}
-      </div>
-      <p className={cn("max-w-6xl text-center", inter.className)}>
-        {description}
-      </p>
-      <div className="flex items-top justify-top gap-2 pt-5">
-        <CyberButton title="Download Game" symbol="DWN" className="w-fit h-11" />
-        {/* <CyberButton title="Download Game" className="w-fit" /> */}
-      </div>
-      <div className="flex items-top justify-top gap-2 pt-5">
-        <CyberButton title="Discord Server" symbol="JOIN" className="w-fit h-12" />
-        {/* <CyberButton title="Discord Server" className="w-fit" /> */}
-      </div>
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import Button from "@/components/modules/home/button";
 import GlitchSection from "@/components/modules/home/glitch-section";
 import Image from "next/image";
@@ -131,12 +39,12 @@ export default function Home() {
           <p className="text-center text-lg font-semibold mb-4">
             <strong>Price:</strong> For free
           </p>
-          
-      <div className="flex items-top justify-top gap-2 pt-5">
-        <CyberButton title="Reserve your nick here" symbol="RESERVE" className="w-fit h-13" />
-        {/* <CyberButton title="Reserve your nick here" className="w-fit" /> */}
-      </div>
-
+          <a
+            href="https://www.cybertankzforum.com/register/"
+            className="reserve-btn px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          >
+            Reserve your nick here
+          </a>
         </div>
 
         {/* Premium Nickname Box */}
